6b8c9aff882b0ae631e97a2bf598d0948b3f28cd
Nanditha S H <shnanditha5@gmail.com>
2026-08-20 13:33:37 +0530

    Day14_change

--- react-portfolio-new/src/components/Navbar/Navbar.jsx
+++ react-portfolio-new/src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar({ theme, toggleTheme }) {
     return (
         <header className="site-header">
             <nav className="navbar">
-                <div className="logo">SD</div>
+                <div className="logo">NH</div>
                 
                 <ul className={isMenuOpen ? "nav-links open" : "nav-links"}>
                     <li>
--- react-portfolio-new/src/pages/NotFound.jsx
+++ react-portfolio-new/src/pages/NotFound.jsx
@@ -3,9 +3,7 @@ import { Link } from 'react-router-dom';
 function NotFound() {
 return (
 <div style={{ textAlign: 'center', padding: '100px' }}>
-<h1>404</h1>
-<p>Page Not Found</p>
-<Link to="/">Back to Home</Link>
+{'404 Page Not FoundBack to Home'}
 </div>
 );
 }
